f8e7a8a3e6d1d054556bbf953f125cff53e36146
Cody Biller <sharpeleven91@gmail.com>
2017-07-12 23:46:40 -0400

    Finish Chapter 1 Exercises

---
+++ Chapter 1/prog2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0;
+    sum = 87 - 15;
+    printf("The sum of 87 minus 15 is %i", sum);
+}
---
+++ Chapter 1/prog3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sum = 0;
+    sum = 25 + 37 - 19;
+    printf("The answer is %i \n", sum);
+    return 0;
+}
